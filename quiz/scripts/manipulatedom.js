cc65f2fdf93bc8b6e96acbfe22d36594c3654451
koeninger.a <koeninger.a@northeastern.edu>
2024-01-29 16:26:32 -0500

    in class monday jan 29

--- quiz/scripts/manipulatedom.js
+++ quiz/scripts/manipulatedom.js
@@ -21,11 +21,29 @@ function createTxtNode(txt) {
   return txtNode;
 }
 
+function editText(node) {
+    let newNode = document.createElement("input");
+    newNode.type = 'text';
+    newNode.placeholder = "Enter Cell (x,y) ...";
+    node.replaceChild(newNode, node.childNodes[0])
+}
+
+function createButtonNode(text, e, l) {
+    let buttonNode = document.createElement("button");
+    let buttonNodeTxt = createTxtNode(text);
+    buttonNode.appendChild(buttonNodeTxt);
+    buttonNode.addEventListener(e, l);
+    return buttonNode;
+}
+
 function addTable() {
   const tableNode = document.createElement("table");
   for(let i = 0; i < 3; i++) {
     let col1 = createTDNode(createTxtNode("Cell (" + i + ", 0)"));
-    tableNode.appendChild(createTRNode([col1]));
+    let col2 = createTDNode(createButtonNode("Edit text", "click", function() {
+        editText(col1);
+        }));
+    tableNode.appendChild(createTRNode([col1, col2]));
   }
   document.getElementById("root").appendChild(tableNode);
 }
